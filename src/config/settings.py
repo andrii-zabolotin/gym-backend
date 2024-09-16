fa8b924bb698963eb8ecaf57b5f00d2af16a6cf6
Andrii Zabolotin <fenics7511@gmail.com>
2024-09-16 15:51:42 +0300

    Create Subscription app

--- src/config/settings.py
+++ src/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "apps.user.apps.UserConfig"
+    "apps.subscriptions.apps.SubscriptionsConfig"
 ]
 
 MIDDLEWARE = [
