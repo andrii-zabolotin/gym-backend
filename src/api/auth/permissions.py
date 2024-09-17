573b3e616c8ca7a985d94f893c2f8114a416930e
Andrii Zabolotin <fenics7511@gmail.com>
2024-09-17 17:07:56 +0300

    Create permissions

---
+++ src/api/auth/permissions.py
@@ -0,0 +1,32 @@
+from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
+
+
+class IsSuperUser(permissions.BasePermission):
+    """The user is a superuser."""
+
+    def has_permission(self, request, view):
+        return (
+            request.user and request.user.is_authenticated and request.user.is_superuser
+        )
+
+
+class IsSuperUserOrReadOnly(permissions.BasePermission):
+    """The user is a superuser or readonly."""
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS or
+            request.user and
+            request.user.is_authenticated and
+            request.user.is_superuser
+        )
+
+
+class IsStaff(permissions.BasePermission):
+    """The user is staff."""
+
+    def has_permission(self, request, view):
+        return (
+            request.user and request.user.is_authenticated and request.user.is_staff
+        )
