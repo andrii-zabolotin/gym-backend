c2c97923e770b1405ced28179ae6a5cf042a9262
Andrii Zabolotin <fenics7511@gmail.com>
2024-09-17 10:39:18 +0300

    Add subscription type to model

--- src/apps/subscriptions/models.py
+++ src/apps/subscriptions/models.py
@@ -10,7 +10,15 @@ class Subscription(models.Model):
         verbose_name = _("Subscription")
         verbose_name_plural = _("Subscriptions")
 
+    SUBSCRIPTION_TYPES = (
+        ('general', 'General'),
+        ('group', 'Group Classes'),
+        ('personal', 'Personal Training'),
+        ('massage', 'Massage'),
+    )
+
     name = models.CharField(verbose_name=_("Subscription name"))
+    subscription_type = models.CharField(max_length=15, choices=SUBSCRIPTION_TYPES, verbose_name=_("Subscription type"))
     validity_period = models.IntegerField(verbose_name=_("Validity period"))
     available_number_of_visits = models.IntegerField(verbose_name=_("Available number of visits"))
     price = models.IntegerField(verbose_name=_("Price"))
@@ -18,7 +26,7 @@ class Subscription(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.name} - {self.price}"
+        return f"{self.name} - {self.price} {self.get_subscription_type_display()}"
 
 
 class UserSubscription(models.Model):
