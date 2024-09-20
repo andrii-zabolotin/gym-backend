c3f2d7bc84717e79c86fea68c59851839f2fc0d7
Andrii Zabolotin <fenics7511@gmail.com>
2024-09-20 15:14:25 +0300

    Add attendance limit and expiration date validation

--- src/apps/attendance/models.py
+++ src/apps/attendance/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
 from apps.subscriptions.models import UserSubscription
 from apps.trainings.models import Training
@@ -13,14 +15,33 @@ class Attendance(models.Model):
         verbose_name = _("Attendance")
         verbose_name_plural = _("Attendances")
 
+    def clean(self):
+        super().clean()
+
+        # Check attendance limit
+        if self.user_subscription.subscription.available_number_of_visits <= Attendance.objects.filter(
+                user_subscription=self.user_subscription).count():
+            raise ValidationError(
+                {
+                    "user_subscription": f"Max visits limit reached ({self.user_subscription.subscription.available_number_of_visits})."
+                }
+            )
+
+        # Check expiration date
+        if self.user_subscription.expiration_at < timezone.now():
+            raise ValidationError(
+                {
+                    "user_subscription": f"Subscription expired at {self.user_subscription.expiration_at}"
+                }
+            )
+
     def __str__(self):
-        return f"Attendance of {self.user_subscription.user.email} on {self.datetime}"
+        return f"Attendance of {self.user_subscription.user.phone} on {self.attendance_time}"
 
 
 class AttendanceTraining(models.Model):
     attendance = models.ForeignKey(Attendance, on_delete=models.PROTECT, verbose_name=_("Attendance"))
     training = models.ForeignKey(Training, on_delete=models.PROTECT, verbose_name=_("Training"))
-    check_in_time = models.DateTimeField(auto_now_add=True, verbose_name=_("Check-in Time"))
 
     class Meta:
         verbose_name = _("Attendance Training")
@@ -28,4 +49,4 @@ class AttendanceTraining(models.Model):
         unique_together = ('attendance', 'training')
 
     def __str__(self):
-        return f"Training {self.training.training_type} for {self.attendance.user} on {self.attendance.attendance_time}"
+        return f"Training {self.training.training_type} for {self.attendance.user_subscription.user.phone} on {self.check_in_time}"
