0b18b458a1681442bde3a178059fe49d6fc99a17
Andrii Zabolotin <fenics7511@gmail.com>
2024-09-20 15:14:40 +0300

    Add endpoints url

--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -1,8 +1,13 @@
 from django.http import JsonResponse
 from django.urls import path, include
 
+from api.auth.auth import CreateTokenView
+
 app_name = "api"
 
 urlpatterns = [
-    path("user/", include("api.v1.user.urls"))
+    path("token/", CreateTokenView.as_view(), name="token"),
+    path("user/", include("api.v1.user.urls")),
+    path("subscription/", include("api.v1.subscription.urls")),
+    path("trainings/", include("api.v1.trainings.urls")),
 ]
