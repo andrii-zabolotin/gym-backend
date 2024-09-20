65915415db880ed96540f2d45239de58ea18526a
Andrii Zabolotin <fenics7511@gmail.com>
2024-09-20 15:13:00 +0300

    Add training model validation

--- src/apps/trainings/models.py
+++ src/apps/trainings/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -7,17 +8,17 @@ from apps.user.models import CustomUser
 
 class Training(models.Model):
     TRAINING_TYPES = (
-        ('personal', 'Personal Training'),
-        ('yoga', 'Yoga'),
-        ('massage', 'Massage'),
-        ('stretching', 'Stretching'),
-        ('trx', 'TRX'),
+        ("personal", "Personal Training"),
+        ("yoga", "Yoga"),
+        ("massage", "Massage"),
+        ("stretching", "Stretching"),
+        ("trx", "TRX"),
     )
 
     description = models.CharField(max_length=255, verbose_name=_("Description"), null=True, blank=True)
     training_type = models.CharField(max_length=15, choices=TRAINING_TYPES, verbose_name=_("Training type"))
     trainer = models.ForeignKey(CustomUser, on_delete=models.PROTECT, verbose_name=_("Trainer"),
-                                limit_choices_to={'is_trainer': True})
+                                limit_choices_to={"is_staff": True})
     date = models.DateField(verbose_name=_("Date"))
     start_time = models.TimeField(verbose_name=_("Start time"))
     end_time = models.TimeField(verbose_name=_("End time"))
@@ -25,8 +26,21 @@ class Training(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def clean(self):
+        super().clean()
+        if self.pk:
+            participants_count = TrainingUser.objects.filter(training=self).count()
+            if self.max_participants is not None and participants_count > self.max_participants:
+                raise ValidationError({
+                    "max_participants": f"Max participants ({self.max_participants}) cannot be less than current participants ({participants_count})."
+                })
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f"{self.training_type} - {self.date} ({self.start_time} - {self.end_time})"
+        return f"{self.get_training_type_display()} - {self.date} ({self.start_time} - {self.end_time})"
 
 
 class TrainingUser(models.Model):
@@ -35,7 +49,21 @@ class TrainingUser(models.Model):
     joined_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Registration date"))
 
     class Meta:
-        unique_together = ('user_subscription', 'training')
+        constraints = [
+            models.UniqueConstraint(fields=["user_subscription", "training"], name="unique_training_user")
+        ]
+
+    def clean(self):
+        super().clean()
+        participants_count = TrainingUser.objects.filter(training=self.training).count()
+        if participants_count >= self.training.max_participants:
+            raise ValidationError({
+                "training": f"The maximum number of participants ({self.training.max_participants}) has already been reached."
+            })
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.user_subscription.user.email} - {self.training.training_type} ({self.training.date})"
+        return f"{self.user_subscription.user.pk} - {self.training.training_type} ({self.training.date})"
