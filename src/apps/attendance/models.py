cda956d07261adb0fb87b7427d8433b6bed2d4aa
Andrii Zabolotin <fenics7511@gmail.com>
2024-09-22 10:57:34 +0300

    Add save method to attendance model

--- src/apps/attendance/models.py
+++ src/apps/attendance/models.py
@@ -35,6 +35,10 @@ class Attendance(models.Model):
                 }
             )
 
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f"Attendance of {self.user_subscription.user.phone} on {self.attendance_time}"
 
