def22a5f961c6d4b6ce93cd26b5de896804705d9
Andrii Zabolotin <fenics7511@gmail.com>
2024-09-22 10:59:16 +0300

    Create attendance serializers

--- src/api/v1/attendance/serializers.py
+++ src/api/v1/attendance/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from apps.attendance.models import Attendance
+from api.v1.subscription.serializers import SubscriptionUserSerializer
+from apps.attendance.models import AttendanceTraining
 
 
-class AttendanceSerializer(serializers.ModelSerializer):
+class AttendanceSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    user_subscription = SubscriptionUserSerializer()
+    attendance_time = serializers.DateTimeField()
+
+
+class AttendanceTrainingSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Attendance
+        model = AttendanceTraining
         fields = "__all__"
