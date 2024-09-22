57440091edef8b824879723ea29e0af738c9a46b
Andrii Zabolotin <fenics7511@gmail.com>
2024-09-22 12:09:59 +0300

    Create subscription views

--- src/api/v1/subscription/views.py
+++ src/api/v1/subscription/views.py
@@ -1,73 +1,66 @@
-from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import extend_schema_view, extend_schema, OpenApiParameter
-from rest_framework import generics, authentication, status
-from rest_framework.response import Response
+from rest_framework import generics, authentication, status, permissions
 
-from api.auth.permissions import IsSuperUser, IsSuperUserOrReadOnly
-from api.v1.subscription.serializers import SubscriptionSerializer, SubscriptionUpdateSerializer
-from apps.subscriptions.models import Subscription
+from api.v1.subscription.filters import SubscriptionFilter, SubscriptionUserFilter
+from api.v1.subscription.serializers import SubscriptionSerializer, SubscriptionUpdateSerializer, \
+    UserSubscriptionSerializer
+from apps.subscriptions.models import Subscription, UserSubscription
 
 
-class CreateSubscriptionView(generics.CreateAPIView):
+class SubscriptionCreateAPIView(generics.CreateAPIView):
     """Create a new subscription in the system"""
 
     serializer_class = SubscriptionSerializer
     authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [IsSuperUser]
+    permission_classes = [permissions.DjangoModelPermissions]
 
 
-@extend_schema_view(
-    get=extend_schema(
-        parameters=[
-            OpenApiParameter(
-                "type",
-                OpenApiTypes.STR,
-                enum=["all", "deleted"],
-            ),
-        ]
-    )
-)
-class ListSubscriptionView(generics.ListAPIView):
+class SubscriptionListAPIView(generics.ListAPIView):
     """Retrieve a list of subscriptions."""
     serializer_class = SubscriptionSerializer
-
-    def get(self, request, *args, **kwargs):
-        type = self.request.query_params.get("type", None)
-        user = self.request.user
-
-        if type:
-            if type not in ['all', 'deleted']:
-                return Response({
-                    "detail": "Invalid type parameter. Must be 'all' or 'deleted'."
-                },
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            elif user.is_active is not True and user.is_superuser is not True:
-                return Response({
-                    "detail": "You do not have permission to perform this action."
-                },
-                    status=status.HTTP_403_FORBIDDEN
-                )
+    authentication_classes = [authentication.TokenAuthentication]
+    filterset_class = SubscriptionFilter
 
     def get_queryset(self):
-        type = self.request.query_params.get("type", None)
+        if not self.request.user.is_anonymous and (self.request.user.is_superuser or self.request.user.is_administrator):
+            return Subscription.objects.all()
 
-        if type:
-            if type == "all":
-                return Subscription.objects.all()
-            elif type == "deleted":
-                return Subscription.objects.only_deleted()
-        else:
-            return Subscription.objects.only_active()
+        return Subscription.objects.filter(is_active=True)
 
 
-class RetrieveUpdateSubscriptionView(generics.RetrieveUpdateAPIView):
+class SubscriptionRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
     """Manage the subscription."""
 
     serializer_class = SubscriptionUpdateSerializer
     authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [IsSuperUserOrReadOnly]
+    permission_classes = [permissions.DjangoModelPermissions]
 
     def get_queryset(self):
-        return Subscription.objects.all()
+        if not self.request.user.is_anonymous and (self.request.user.is_superuser or self.request.user.is_administrator):
+            return Subscription.objects.all()
 
+        return Subscription.objects.filter(is_active=True)
+
+
+class UserSubscriptionListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = UserSubscriptionSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions]
+    filterset_class = SubscriptionUserFilter
+
+    def get_queryset(self):
+        if self.request.user.is_superuser or self.request.user.is_administrator:
+            return UserSubscription.objects.all()
+
+        return UserSubscription.objects.filter(user=self.request.user)
+
+
+class UserSubscriptionRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSubscriptionSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions]
+
+    def get_queryset(self):
+        if self.request.user.is_superuser or self.request.user.is_administrator:
+            return UserSubscription.objects.all()
+
+        return UserSubscription.objects.filter(user=self.request.user)
