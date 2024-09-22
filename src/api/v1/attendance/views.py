da0ecd515ab3efae916bc00c186f3a68fa2190c1
Andrii Zabolotin <fenics7511@gmail.com>
2024-09-22 10:59:39 +0300

    Create attendance views

--- src/api/v1/attendance/views.py
+++ src/api/v1/attendance/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from rest_framework import generics, status, authentication, permissions
+from rest_framework.response import Response
 
-# Create your views here.
+from api.v1.attendance.filters import AttendanceFilter, AttendanceTrainingFilter
+from api.v1.attendance.serializers import AttendanceSerializer, AttendanceTrainingSerializer
+from apps.attendance.models import Attendance, AttendanceTraining
+
+
+class AttendanceListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = AttendanceSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions]
+    filterset_class = AttendanceFilter
+
+    def get_queryset(self):
+        if self.request.user.is_superuser or self.request.user.is_administrator:
+            return Attendance.objects.all()
+
+        return Attendance.objects.filter(user_subscription__user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        try:
+            return super().create(request, *args, **kwargs)
+        except ValidationError as e:
+            return Response(
+                e.message_dict,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class AttendanceRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = AttendanceSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions]
+    queryset = Attendance.objects.all()
+
+    def get_queryset(self):
+        if self.request.user.is_superuser or self.request.user.is_administrator:
+            return Attendance.objects.all()
+
+        return Attendance.objects.filter(user_subscription__user=self.request.user)
+
+
+class AttendanceTrainingListCreateAPIView(generics.ListCreateAPIView):
+    queryset = AttendanceTraining.objects.all()
+    serializer_class = AttendanceTrainingSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions]
+    filterset_class = AttendanceTrainingFilter
+
+    def get_queryset(self):
+        if self.request.user.is_superuser or self.request.user.is_administrator:
+            return AttendanceTraining.objects.all()
+
+        return AttendanceTraining.objects.filter(attendance__user_subscription__user=self.request.user)
+
+
+class AttendanceTrainingRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = AttendanceTraining.objects.all()
+    serializer_class = AttendanceTrainingSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions]
+
+    def get_queryset(self):
+        if self.request.user.is_superuser or self.request.user.is_administrator:
+            return AttendanceTraining.objects.all()
+
+        return AttendanceTraining.objects.filter(attendance__user_subscription__user=self.request.user)
