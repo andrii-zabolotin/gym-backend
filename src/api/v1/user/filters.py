762586c02d21b1b10234dd343f199ce42a4cbed0
Andrii Zabolotin <fenics7511@gmail.com>
2024-10-13 07:25:31 +0300

    Create filter for User model

---
+++ src/api/v1/user/filters.py
@@ -0,0 +1,14 @@
+from django_filters import rest_framework as filters
+
+
+class UserFilter(filters.FilterSet):
+    group = filters.ChoiceFilter(field_name="groups", choices=[
+        (1, "Тренери"),
+        (2, "Масажисти"),
+    ])
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.data = self.data.copy()
+        if 'group' not in self.data:
+            self.data['group'] = 2
