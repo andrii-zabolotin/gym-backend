05795b1f4bb483bd2f9414381b12320db679c29f
Andrii Zabolotin <fenics7511@gmail.com>
2024-10-14 11:45:54 +0300

    Add default pagination

--- src/config/settings.py
+++ src/config/settings.py
@@ -69,6 +69,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
 }
 
 CORS_ALLOWED_ORIGINS = [
