96aeedc2b2dd432f36c1dc5b610965d39b2d0e30
Andrii Zabolotin <fenics7511@gmail.com>
2024-10-14 11:46:40 +0300

    Create new user endpoints

--- src/api/v1/user/urls.py
+++ src/api/v1/user/urls.py
@@ -1,12 +1,14 @@
-from django.http import JsonResponse
-from django.urls import path, include
+from django.urls import path
 
-from api.v1.user.views import CreateUserView, ManageUserView, UserListAPIView
+from api.v1.user.views import CreateUserView, ManageUserView, UserStaffListAPIView, UserRetrieveUpdateAPIView, \
+    UserGetAPIView
 
 app_name = "api_user"
 
 urlpatterns = [
     path("", CreateUserView.as_view(), name="user-create"),
-    path("list/", UserListAPIView.as_view(), name="user-list"),
+    path("<int:pk>/", UserRetrieveUpdateAPIView.as_view(), name="user-retrieve"),
+    path("staff/", UserStaffListAPIView.as_view(), name="user-staff"),
+    path("list/", UserGetAPIView.as_view(), name="user-list"),
     path("me/", ManageUserView.as_view(), name="user-manage"),
 ]
--- src/api/v1/user/views.py
+++ src/api/v1/user/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics, authentication, permissions
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import generics, authentication, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from api.v1.user.filters import UserFilter
+from api.auth.permissions import IsAdministratorOrSuperUser
+from api.v1.user.filters import StaffFilter
 from api.v1.user.serializers import UserSerializer, LimitedUserSerializer
 
 
@@ -24,9 +28,51 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
         return self.request.user
 
 
-class UserListAPIView(generics.ListAPIView):
+class UserStaffListAPIView(generics.ListAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = LimitedUserSerializer
     authentication_classes = []
     permission_classes = []
-    filterset_class = UserFilter
+    filterset_class = StaffFilter
+
+
+class UserGetAPIView(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [IsAdministratorOrSuperUser]
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(name='phone', description='User phone number', required=False, type=str),
+            OpenApiParameter(name='email', description='User email address', required=False, type=str),
+            OpenApiParameter(name='id', description='User ID', required=False, type=int),
+        ],
+        responses={
+            200: UserSerializer,
+            404: OpenApiParameter(name="error", description="User not found", required=False, type=str),
+        },
+    )
+    def get(self, request):
+        phone = request.query_params.get('phone')
+        email = request.query_params.get('email')
+        user_id = request.query_params.get('id')
+
+        user = None
+        if user_id:
+            user = get_user_model().objects.filter(id=user_id).first()
+        if phone:
+            user = get_user_model().objects.filter(phone=phone).first()
+        if email:
+            user = get_user_model().objects.filter(email=email).first()
+
+        if not user:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.DjangoModelPermissions]
