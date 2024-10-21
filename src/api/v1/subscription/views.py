c502cedacd8a575da1b03d339e6ada309db3f78d
Andrii Zabolotin <fenics7511@gmail.com>
2024-10-21 11:25:14 +0300

    Add SubscriptionTypeViewSet

--- src/api/v1/subscription/views.py
+++ src/api/v1/subscription/views.py
@@ -1,17 +1,24 @@
-from rest_framework import generics, authentication, status, permissions
+from rest_framework import generics, authentication, status, permissions, viewsets
+from rest_framework.permissions import SAFE_METHODS
 
+from api.auth.permissions import IsSuperUser
 from api.v1.subscription.filters import SubscriptionFilter, SubscriptionUserFilter
 from api.v1.subscription.serializers import SubscriptionSerializer, SubscriptionUpdateSerializer, \
-    UserSubscriptionSerializer
-from apps.subscriptions.models import Subscription, UserSubscription
+    UserSubscriptionSerializer, CustomUserSubscriptionSerializer, SubscriptionTypeSerializer, \
+    CustomSubscriptionSerializer
+from apps.subscriptions.models import Subscription, UserSubscription, SubscriptionType
 
 
 class SubscriptionListCreateAPIView(generics.ListCreateAPIView):
     """Retrieve a list of subscriptions."""
-    serializer_class = SubscriptionSerializer
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = []
     filterset_class = SubscriptionFilter
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return CustomSubscriptionSerializer
+        return SubscriptionSerializer
 
     def get_permissions(self):
         if self.request.method == 'POST':
@@ -41,19 +48,23 @@ class SubscriptionRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
 
 
 class UserSubscriptionListCreateAPIView(generics.ListCreateAPIView):
-    serializer_class = UserSubscriptionSerializer
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions]
     filterset_class = SubscriptionUserFilter
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return CustomUserSubscriptionSerializer
+        return UserSubscriptionSerializer
 
     def get_queryset(self):
         if self.request.user.is_anonymous:
             return UserSubscription.objects.none()
 
         if self.request.user.is_superuser or self.request.user.is_administrator:
-            return UserSubscription.objects.all()
+            return UserSubscription.objects.all().order_by("-purchase_at")
 
-        return UserSubscription.objects.filter(user=self.request.user)
+        return UserSubscription.objects.filter(user=self.request.user).order_by("-purchase_at")
 
 
 class UserSubscriptionRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
@@ -66,3 +77,17 @@ class UserSubscriptionRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestro
             return UserSubscription.objects.all()
 
         return UserSubscription.objects.filter(user=self.request.user)
+
+
+class SubscriptionTypeViewSet(viewsets.ModelViewSet):
+    queryset = SubscriptionType.objects.all()
+    serializer_class = SubscriptionTypeSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+
+    def get_permissions(self):
+        if self.request.method in SAFE_METHODS:
+            self.permission_classes = []
+        else:
+            self.permission_classes = [IsSuperUser]
+
+        return super().get_permissions()
